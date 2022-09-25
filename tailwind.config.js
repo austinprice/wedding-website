62b6f354aef5ea3167afc5122bf98a6d50803e02
Austin Price <priceaustinw@gmail.com>
2022-09-25 19:55:13 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      padding: '2rem',
+    },
+    extend: {
+      colors: {
+        'green': '#20503D',
+        'red': '#BC0606',
+        'tan-light': '#F2EFE6',
+        'tan-mid': '#D3CFC3',
+        'tan-dark': '#A49E8C'
+      },
+      fontFamily: {
+        'display': ['Bebas Neue', 'Arial', 'sans-serif'],
+        'script': ['Tilda', 'sans-serif'],
+        'hagerman': ['Hagerman', 'Arial', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
